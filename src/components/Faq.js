ae1a963e930fea8336f03254f832256604c5e62f
“aymenkhs” <aymenkhouas@gmail.com>
2021-12-17 16:07:36 +0100

    fixing fac html

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -3,7 +3,6 @@ import { FaQuestion, FaMapMarkerAlt } from 'react-icons/fa';
 import { BiTime } from 'react-icons/bi';
 import { AiOutlineUnorderedList, AiOutlineEye, AiFillMail, AiFillBook } from 'react-icons/ai';
 
-
 const Faq = () => {
     return (
         <div className='faq' style={{
@@ -17,8 +16,8 @@ const Faq = () => {
             <br/>
             <div className='row'>
                 <div>
+                <h3 className='title blue'> > Academic leave</h3>
                     <ul>
-                        <li>Academic leave</li>
                         <li><BiTime /> Schedules L1</li>
                         <li><BiTime /> Schedules L2 L3 M1 M2</li>
                         <li><AiOutlineUnorderedList /> Know my Section</li>
@@ -26,8 +25,8 @@ const Faq = () => {
                     </ul>
                 </div>
                 <div>
+                <h3 className='title blue'> > Needing help</h3>
                     <ul>
-                        <h3 className='title blue'> Needing help</h3>
                         <li><FaMapMarkerAlt /> Plan of teaching premises</li>
                         <li><FaMapMarkerAlt /> Plan of blocks 100,200,300,400bis</li>
                         <li><FaQuestion /> How to create an account</li>
@@ -35,7 +34,7 @@ const Faq = () => {
                     </ul>
                 </div>
             </div>
-            <h3 className='title blue'>You can access to the same login and password</h3>
+            <h3 className='title blue'> > You can access to the same login and password</h3>
             <ul>
                 <li><AiFillMail /> To the mailbox</li>
                 <li><AiFillBook />To the online lessons</li>
