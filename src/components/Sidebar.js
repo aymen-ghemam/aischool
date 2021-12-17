9103930005f8fb4f07280c1b2a1c8b70c632db9d
“aymenkhs” <aymenkhouas@gmail.com>
2021-12-17 20:34:41 +0100

    add app and sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,32 @@
 import React from 'react'
+import { BsFillEyeFill } from 'react-icons/bs';
+import { FaThList } from 'react-icons/fa';
+import { AiFillMail } from 'react-icons/ai';
 
 const SideBar = (props) => {
     return (
         <div className='side-bar'>
-            <div>
-                <h3 className='name'> {props.name} </h3>
-                <h5 className='sub-name'> {props.subContent} </h5>
+            <div className='side-bar-name'>
+                <ul>
+                    <li className='name'>{props.name} </li>
+                    <li className='sub-name'>{props.subContent} </li>
+                </ul>
             </div>
             <div className='side-bar-menu'>
-                <h5 className=''> TIMETABLE </h5>
-                <h5 className=''> SCORE BUILTIN </h5>
-                <h5 className=''> REQUEST DOCUCMENTS OR TAKE APOINTEMENTS </h5>
+                <ul>
+                    <li className='elements'>
+                        <p className='icon'><FaThList /></p>
+                        <p className='elem'>TIMETABLE</p>
+                    </li>
+                    <li className='elements'>
+                        <p className='icon'><BsFillEyeFill /></p>
+                        <p className='elem'>SCORE BUILTIN</p>
+                    </li>
+                    <li className='elements'>
+                        <p className='icon'><AiFillMail /></p>
+                        <p className='elem'>REQUEST DOCUCMENTS OR TAKE APOINTEMENTS</p>
+                    </li>
+                </ul>
             </div>
         </div>
     )
